65aa39c7e47a843ce43b0da4ef68074743b7220a
albert.sigman <albert.sigman@zenecosystems.com>
2022-06-14 08:58:50 -0400

    updated production

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -3,50 +3,6 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 const fs = require('fs')
-// async function main() {
-//   Promise.all(
-//     vendors.map(async (v) => {
-//       const salt = await bcrypt.genSalt(Number(String(process.env.SALT_NUMBER)))
-//       const pw = await bcrypt.hash('admin', salt)
-//       const email = `${v.vendorId}@${v.vendorName}.com`.toLowerCase()
-//       const data = {
-//         ...v,
-//         user: {
-//           email,
-//           password: pw,
-//           firstName: `${v.vendorId}`,
-//           lastName: `${v.vendorName}`,
-//         },
-//       }
-//       return data
-//     }),
-//   ).then(async (vendors) => {
-//     console.log(`Start seeding ...`)
-//     for (const v of vendors) {
-//       const vendor = await prisma.vendor
-//         .create({
-//           data: v,
-//         })
-//         .catch((err) => console.error(err))
-//       console.log('now making users: ', v.user)
-//       const users = await prisma.user
-//         .create({
-//           data: {
-//             email: v.user.email,
-//             vendorId: v.vendorId,
-//             password: v.user.password,
-//             firstName: v.user.firstName,
-//             lastName: v.user.lastName,
-//             roles: 4,
-//           },
-//         })
-//         .catch((err) => console.error(err))
-//       console.log(`Created vendor with id: ${vendor.id}`, users)
-//     }
-//     // await createAdmin()
-//     console.log(`Seeding finished.`)
-//   })
-// }
 
 async function createAdmin() {
   const salt = await bcrypt.genSalt(Number(String(process.env.SALT_NUMBER)))
@@ -98,14 +54,6 @@ async function createAdmin() {
   })
 }
 
-// main()
-//   .catch((e) => {
-//     console.error('error: ' + e)
-//     process.exit(1)
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect()
-//   })
 createAdmin()
   .then((res) => console.log('res: ', res))
   .finally(async () => {
